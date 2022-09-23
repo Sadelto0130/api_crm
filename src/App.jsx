4dad6dfccc18ec2ba0de5c59e8bfbeb17b442206
sadelto0130 <vsalaz00@live.com>
2022-09-23 01:06:57 -0300

    CRM - React

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route}from 'react-router-dom'
+import Layout from './layout/Layout'
+import Inicio from './pages/Inicio'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
+import VerClientes from './pages/VerClientes'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>        
+        <Route path='/clientes' element={<Layout />}>
+          <Route index element={<Inicio />}/>
+          <Route path='nuevo' element={<NuevoCliente />}/>
+          <Route path='editar/:id' element={<EditarCliente />}/>
+          <Route path=':id' element={<VerClientes />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
